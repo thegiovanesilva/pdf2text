499e0256abc0db51a3674f8d25db75f779e468bb
Giovane Goncalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2022-02-15 14:39:26 -0300

    Fix bugs and added Readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # pdf2text
-An api that extract text from a pdf file
+An api that extract text from a pdf file.
+
+# Requirements
+Python 3.6+
+
+# Instalation
+```bash
+pip install fastapi
+pip install pdfminer
+pip install shutil
+pip install uuid
+pip install "uvicorn[standard]"
+```
+
+# Running project
+```bash
+uvicorn main:app
+```
+
+# Documentation
+You will see the automatic interactive API documentation (after running) at http://127.0.0.1:8000/docs.
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import PyPDF2
 from fastapi import FastAPI, File, UploadFile
+from os import remove
+from pdfminer.high_level import extract_text 
+from shutil import copyfileobj
+from uuid import uuid4
 
 app = FastAPI()
 
 @app.post('/extract/')
 def extract(file: UploadFile = File(...)):
-    pdfFileObj = open(file.filename,'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    pagehandle = pdfReader.getPage(0)
-    text = pagehandle.extractText()
-    return {'text': text}
+    filename = str(uuid4())
+    with open(filename,'wb') as buffer:
+        copyfileobj (file.file, buffer)
+    text = extract_text(filename, "", [0])
+    remove(filename)
+    return { 'text': text }
